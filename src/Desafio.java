14e7c9b0bac69c18b2e66af70e4236a01e3406c3
Thiago Meireles <thiago22md@gmail.com>
2024-02-28 11:15:12 -0300

    menu do cliente

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nome = "Thiago Meireles";
         String tipoConta = "Conta Corrente";
         double saldo = 1599.99;
+        int opcao = 0;
 
         System.out.println("**********************");
         System.out.println("\nNome do cliente: " + nome);
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo Atual: " + saldo);
         System.out.println("\n**********************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
+
     }
 }
