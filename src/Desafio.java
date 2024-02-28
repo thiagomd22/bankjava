a74ce36bf50c164458a1f30295a689fae3416591
Thiago Meireles <thiago22md@gmail.com>
2024-02-28 11:02:43 -0300

    inicializar dados d cliente

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Thiago Meireles";
+        String tipoConta = "Conta Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("**********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo Atual: " + saldo);
+        System.out.println("\n**********************");
+    }
+}
